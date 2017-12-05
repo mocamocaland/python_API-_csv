cc30a2fd9f0b7b701f78866f37b2611aa0a07f29
mocamoca <notoya@cnctor.jp>
2017-12-05 14:34:56 +0900

    投稿一覧にタイトルが表示されるようにする

--- myblogapp/posts/models.py
+++ myblogapp/posts/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    published = models.DateField()
+    published = models.DateTimeField()
     image = models.ImageField(upload_to='media/')
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
