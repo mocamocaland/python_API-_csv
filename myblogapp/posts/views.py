f0702dc5356441a465bf5bb4e2466d58e8809428
mocamoca <notoya@cnctor.jp>
2017-12-05 15:01:00 +0900

    HTMLに投稿一覧を表示

--- myblogapp/posts/views.py
+++ myblogapp/posts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+
 # Create your views here.
 
 def index(request):
     # return HttpResponse("Hello World!")
-    return render(request, 'posts/index.html')
+    posts = Post.objects.order_by('-published')
+    return render(request, 'posts/index.html', {'posts':posts})
 
